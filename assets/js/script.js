4fa23c6a2d006b77c710f3c43591a4785fb570b9
vince4dev <vincent.bollier@icloud.com>
2026-05-29 00:16:13 +0200

    Added CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
-// RANGE
+// ---=== CURSOR (RANGE) ===---
 const cursor = document.getElementById("cursor");
 const cursorValue = document.getElementById("cursor-value");
 
+// * Cursor Value
 cursor.addEventListener("input", () => {
   cursorValue.textContent = cursor.value;
 });
+
+// * Color range track
+function updateCursor() {
+  const percent =
+    ((cursor.value - cursor.min) / (cursor.max - cursor.min)) * 100;
+  // on met la variable CSS sur l’élément <input>
+  cursor.style.setProperty("--cursor-percentage", percent + "%");
+}
+cursor.addEventListener("input", updateCursor);
+
+// initialisation
+updateCursor();
